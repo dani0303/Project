1973eb86ffe01c5c30fc33c98c564097b382d478
rodger2003 <48264113+dani0303@users.noreply.github.com>
2021-12-04 20:45:23 -0500

    Update pwdCreator.py

--- PasswordMachine/pwdCreator.py
+++ PasswordMachine/pwdCreator.py
@@ -3,7 +3,7 @@ class Generator:
     def __init__ (self, pwd):#pwd is a password passed as a string
         self.pwd = pwd
 
-    def create_new_pwd(pwd):
+    def new_pwd(pwd):
         numChar = len(pwd)#gets length of the string
         rand = random.randrange(0, numChar/2)#get a random num within parameters
         p1 = pwd[rand:]#gets substring
