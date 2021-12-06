c4b14306c9f468fe4e809c83a1638e5a9dfdbb28
rodger2003 <48264113+dani0303@users.noreply.github.com>
2021-12-06 03:48:51 +0000

    Update gui.py

--- PasswordMachine/gui.py
+++ PasswordMachine/gui.py
@@ -1,20 +1,16 @@
 import pyfiglet
-from pwdCreator import Generator
+from pwdCreator import Creator
 
 title = pyfiglet.figlet_format("Welcome to the PasswordEater", font="slant")
 
+
 print("**************************************************************************************************************************\n")
-print(title)
+print(title)  
+print("==========================================================================================================================\n")
+print("Created by Daniel Rodriguez 12/4/2021")
 print("**************************************************************************************************************************\n")
 
 while True:
-    try:
-        password = input("Enet your current password (making it long): ")
-        if len(password) <= 1:
-            raise ValueError
-        else:
-            pwd = Generator(password)
-            print(pwd.new_pwd())
-
-    except ValueError:
-        print("Give me a password that will fit!")
+    password = input("Enet your current password (making it long): ")
+    pwd = Creator(password)
+    print(pwd.new_pwd())
