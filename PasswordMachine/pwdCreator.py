ef016427307468de552bff1476ef2f292b06ddbf
rodger2003 <48264113+dani0303@users.noreply.github.com>
2021-12-06 03:49:06 +0000

    Update pwdCreator.py

--- PasswordMachine/pwdCreator.py
+++ PasswordMachine/pwdCreator.py
@@ -1,11 +1,17 @@
+"""
+What I would like to do next is making it spit out the changed password
+but have the same amount of characters from the first one
+"""
+
 import random
-class Generator:
+class Creator:
     def __init__ (self, pwd):#pwd is a password passed as a string
         self.pwd = pwd
 
-    def new_pwd(pwd):
-        numChar = len(pwd)#gets length of the string
-        rand = random.randrange(0, numChar/2)#get a random num within parameters
-        p1 = pwd[rand:]#gets substring
-        new_password = p1 + pwd
-        return new_password
+    def new_pwd(self):
+        numChar = len(self.pwd)
+        rand = random.randrange(0, numChar)#get a random num within parameters
+        p1 = self.pwd[rand:]#gets substring
+        p2 = self.pwd[int(rand/2):]
+        new_password = p1 + p2
+        return "new password is " + new_password
